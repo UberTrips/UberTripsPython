a33338ed1d29d71514b2c759d11524bd9ac13fec
Hudson Cavanagh <hucavanagh@gmail.com>
2016-08-04 15:44:31 -0400

    working flask mvp pushed

--- uber_app/uber_app_mvp.py
+++ uber_app/uber_app_mvp.py
@@ -8,45 +8,40 @@ import sklearn
 
 #MODEL GOES HERE
 
-# df = pd.read_csv('/Users/HudsonCavanagh/Documents/titanic.csv') #training data
-
-# # Create dummies and drop NaNs
-# df['Sex'] = df['Sex'].apply(lambda x: 0 if x == 'male' else 1)
-# df = df[include].dropna()
-#
-# X = df[['Pclass', 'Sex', 'Age', 'Fare', 'SibSp']]
-# y = df['Survived']
-
-# PREDICTOR = .75
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 
 
-#http://127.0.0.1:4000/predict?hood_id=2&day_period=1&day_of_week=2
+trips = pd.read_csv('/Users/HudsonCavanagh/GA_dsi-projects/UberTripPython/uber_app/non_uber_trips.csv')
+trips = trips.iloc[:,1:]
+trips.columns = ['weekday', 'hour', 'taxi-cs_avg_trips', 'uber_avg_trips','lyft_avg_trips', 'total_trips', 'uber_prob', 'uber_taxi_bin']
+lr_basic = LogisticRegression(solver='liblinear', penalty='l2', C=0.75)
+
+X = trips.iloc[:,0:2]
+
+y = trips['uber_taxi_bin']
+PREDICTOR =lr_basic.fit(X, y)
+
+
+#http://127.0.0.1:4000/predict?hour=3&day_of_week=2
 # routes go here
 @app.route('/predict', methods=["POST", "GET"]) #, methods=['GET']
 def predict():
-
-    #mvp input below - commented out future inputs
-
-    #
-    hood_id = int(flask.request.args['hood_id'])
-    day_period = int(flask.request.args['day_period'])
+    # hood_id = int(flask.request.args['hood_id'])
+    hour = int(flask.request.args['hour'])
     day_of_week = int(flask.request.args['day_of_week'])
 
     # address = inputs['address'][0]
     # time_of_day = inputs['time_of_day'][0]
 
-
-    item_mvp = np.array([day_period, hood_id,day_of_week])
-
-
-    # item = np.array([address, time_of_day, fare, sibsp])
-    # score_mvp = PREDICTOR.predict_proba(item_mvp)
-    # score = PREDICTOR.predict_proba(item)
-    # results_mvp = {'survival chances': score_mvp[0,1]}
+    item_basic = [hour, day_of_week]
+    # item_more = np.array([day_period, hood_id,day_of_week])
 
 
-    fake_results = {'taxi_prob':list(item_mvp)}
-    return flask.jsonify(fake_results)
+    score_mvp = PREDICTOR.predict_proba(item_basic)
+    results_mvp = {'Chances of an Uber': score_mvp[0][0], 'Chances of Taxi or Car Service':score_mvp[0][1]} #[0,1]
+    # results_test = {'Chances of an Uber': item_basic} #[0,1]
+
+    return flask.jsonify(results_mvp)
 
 # alternate routes
 
@@ -54,25 +49,6 @@ def predict():
 def page():
     with open("uber_page.html", 'r') as viz_file:
         return viz_file.read()
-
-
-@app.route('/result', methods=['POST', 'GET'])
-def result():
-    '''Gets prediction using HTML form'''
-    if flask.request.method == 'POST':
-        inputs = flask.request.form
-        #mvp input below - commented out future inputs
-        hood_id = inputs['hood_id'][0]
-        day_period = inputs['day_period'][0]
-        # address = inputs['address'][0]
-        # time_of_day = inputs['time_of_day'][0]
-        item_mvp = np.array([day_period, hood_id])
-        # item = np.array([address, time_of_day, fare, sibsp])
-        score_mvp = PREDICTOR.predict_proba(item_mvp)
-        # score = PREDICTOR.predict_proba(item)
-        results_mvp = {'survival chances': score_mvp[0,1]}
-        fake_results = {'taxi_prob':.7}
-        return flask.jsonify(fake_results)
 
 
 if __name__ == '__main__':
